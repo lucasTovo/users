ab0a337b02b594f975777ad12530393df0c2bb45
guismoreira <guilhermemoreira55@gmail.com>
2020-06-21 20:34:16 -0300

    #6

--- src/main/java/orion/user/util/LoginServlet.java
+++ src/main/java/orion/user/util/LoginServlet.java
@@ -9,62 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
  
-@WebServlet("/loginServlet")
+@WebServlet("/servlet")
 public class LoginServlet extends HttpServlet {
  
-        /**
-         *
-         */
+
         private static final long serialVersionUID = 1L;
 
-        public void doGet(
-                HttpServletRequest request,
-                HttpServletResponse response) throws ServletException, IOException {
-
-                 // read form fields
+        @Override 
+        protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+        
                 String email = request.getParameter("email");
-                String password = request.getParameter("password");
-
-                        // do some processing here...
-         
-                // get response writer
-                PrintWriter writer = response.getWriter();
-                
-                // build HTML code
-                String htmlRespone = "<html>";
-                htmlRespone += "<h2>Your email is: " + email + "<br/>";      
-                htmlRespone += "Your password is: " + password + "</h2>";    
-                htmlRespone += "</html>";
-                
-                // return response
-                writer.println(htmlRespone);
-                }
-
-        protected void doPost(
-                        HttpServletRequest request,
-                        HttpServletResponse response) throws ServletException, IOException {
-         
-        // read form fields
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-         
-        System.out.println("email: " + email);
-        System.out.println("password: " + password);
- 
-        // do some processing here...
-         
-        // get response writer
-        PrintWriter writer = response.getWriter();
-         
-        // build HTML code
-        String htmlRespone = "<html>";
-        htmlRespone += "<h2>Your email is: " + email + "<br/>";      
-        htmlRespone += "Your password is: " + password + "</h2>";    
-        htmlRespone += "</html>";
-         
-        // return response
-        writer.println(htmlRespone);
-         
-    }
+                PrintWriter out = response.getWriter();
+                out.println("<html><body>Empresa " + email + " cadastrada</body></html>");
+                System.out.println(email);
+        }
  
 }
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,91 +1,16 @@
-Vue.component('signUpForm',{
-	template:'#signUpForm',
-	data(){
-		return {
-			password:'',
-			confirmPassword:'',
-			email:'',
-			msg:[],
-			disableSubmitButton : true,
-		}
-	},
-	watch:{
-		email(value){
-			this.eventName();
-			this.email = value;
-			this.check_email(value);
-		},
-		password(value){
-			this.eventName();
-			this.password = value;
-			this.checkPassword(value);
-		},
-		confirmPassword(value){
-			this.eventName();
-			this.confirmPassword = value;
-			this.checkConfirmPassword(value);
-		}
-	},
-	methods:{
-		changeToTnc(){
-			this.$emit('change','tnc');
-		},
-		check_email(value){
-			if (/^\w+([\.-]?\ w+)*@\w+([\.-]?\ w+)*(\.\w{2,3})+$/.test(value))
-			{
-				this.msg[name] = '';
-			}else{
-				this.msg[name] = 'Keep Typing... We are waiting for correct Email';
-			}
-		},
-		checkPassword(value){
-			this.passwordLengthCheck(value);
-		},
-		checkConfirmPassword(value){
-			if(this.passwordLengthCheck(value)){
-				if (value == this.password) {
-					this.msg[name]= '';
-					this.disableSubmitButton = false;
-				}else{
-					this.msg[name] = "Password does not match, please try again";
-				}
-			}
-		},
-		passwordLengthCheck(passwordToCheck){
-			remainingChars = 6 - passwordToCheck.length;
-			if (passwordToCheck.length < 6) {
-				this.msg[name] = 'Password must contain 6 characters. '+ remainingChars +' more to go...';
-			}else{
-				this.msg[name] ='';
-				return true;
-			}
-		},
-		eventName(){
-			name = event.target.name;
-		},
-		submit(){
-			window.location.href = "loginServlet";
-		}
-	}
-});
+// grab the things we need
+const signupForm = document.getElementById('signup-form');
+const nameInput  = signupForm.querySelector('input[name=name]');
+const emailInput = signupForm.querySelector('input[name=email]');
 
-Vue.component('tnc',{
-	template:'#tnc',
-	methods:{
-		back_to_signup(){
-			this.$emit('change','signUpForm');
-		}
-	}
-});
+// listen for the submit event
+signupForm.addEventListener('submit', processSignupForm);
+function processSignupForm(e) {
+  e.preventDefault();
 
-new Vue({
-	el:'#app',
-	data:{
-		componentName:'signUpForm'
-	},
-	methods:{
-		change(newComp){
-			this.componentName = newComp;
-		}
-	}
-})
+  const name = nameInput.value;
+  const email = emailInput.value;
+
+  console.log({ name, email });
+  alert('Processing!');
+}
