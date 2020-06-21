5e7500883067b36f121e3ae03fa0d180b2a5fc47
guismoreira <guilhermemoreira55@gmail.com>
2020-06-21 20:37:20 -0300

    #6

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,16 +1,13 @@
-// grab the things we need
-const signupForm = document.getElementById('signup-form');
-const nameInput  = signupForm.querySelector('input[name=name]');
-const emailInput = signupForm.querySelector('input[name=email]');
-
-// listen for the submit event
-signupForm.addEventListener('submit', processSignupForm);
-function processSignupForm(e) {
-  e.preventDefault();
-
-  const name = nameInput.value;
-  const email = emailInput.value;
-
-  console.log({ name, email });
-  alert('Processing!');
-}
+const app = new Vue({
+	el: '#signup-form',
+	data: {
+	  name: '',
+	  email: ''
+	},
+	methods: {
+	  processForm: function() {
+		console.log({ name: this.name, email: this.email });
+		alert('Processing');
+	  }
+	}
+  });
