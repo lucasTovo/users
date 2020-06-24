b42f9c51c9b7a26daff1a49ec49aa3a9f1a3ed25
guismoreira <guilhermemoreira55@gmail.com>
2020-06-24 00:26:39 -0300

    #6

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,13 +1,50 @@
+var check = function() {
+    if (document.getElementById('pw1').value ==
+      document.getElementById('pw2').value) {
+      document.getElementById('message').style.color = 'green';
+      document.getElementById('message').innerHTML = 'matching';
+    } else {
+      document.getElementById('message').style.color = 'red';
+      document.getElementById('message').innerHTML = 'not matching';
+    }
+  }
+
+
+  function manage(email) {
+    var bt = document.getElementById('btSubmit');
+    if (email.value != '') {
+        bt.disabled = false;
+    }
+    else {
+        bt.disabled = true;
+    }
+} 
+
+function managepw1(pw1) {
+    var bt = document.getElementById('btSubmit');
+    if (pw1.value != '') {
+        bt.disabled = false;
+    }
+    else {
+        bt.disabled = true;
+    }
+}    
+
+function managepw2(pw2) {
+    var bt = document.getElementById('btSubmit');
+    if (pw2.value != '') {
+        bt.disabled = false;
+    }
+    else {
+        bt.disabled = true;
+    }
+}    
+
 const app = new Vue({
 	el: '#signup-form',
 	data: {
-	  name: '',
-	  email: ''
-	},
-	methods: {
-	  processForm: function() {
-		console.log({ name: this.name, email: this.email });
-		alert('Processing');
-	  }
-	}
+	  email: '',
+	  password: ''
+    }
+    
   });
