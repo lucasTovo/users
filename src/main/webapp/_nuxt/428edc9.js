d9c22e6d1103a1e2cb4e9f3380451eb8a345945e
guismoreira <guilhermemoreira55@gmail.com>
2020-10-14 17:33:53 -0300

    #10

---
+++ src/main/webapp/_nuxt/428edc9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{370:function(t,e,n){"use strict";n.r(e);var o=n(51),component=Object(o.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v(this._s(this.$t("home.footer")))])}),[],!1,null,null,null);e.default=component.exports}}]);
